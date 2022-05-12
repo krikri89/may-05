4860941a9f6ec2f9565c80e06d369a956ba7ec8e
Kristina Rudyte <rkristina32@yahoo.com>
2022-05-12 11:31:20 +0300

    function

--- functions.php
+++ functions.php
@@ -1,1 +1,49 @@
 <?php
+const br = '<br>';
+echo br.'----------------------'.br;
+
+$valio = 'Valio!';
+$kitas = 'Kitas';
+
+function fun ($hhh, $kkk) {
+    echo ' FUN!!!!';
+    echo '$kkk';
+    echo br;
+    echo '$hhh';
+    // global $valio, $kitas; // not to use
+    $atgal =123;
+    return $atgal;
+    
+}
+// funkcijos iskvietimas
+$atgal = fun($valio, $kitas);
+echo br;
+echo $atgal;
+echo br.'----------------------'.br;
+
+function daugiklis(...$m) {
+    $rez =$m[0] *$m[1] *$m[2];
+    return $rez;
+}
+
+$mas = [5, 5, 10];
+echo daugiklis(6, 6, 10);
+
+echo br.'----------------------'.br;
+
+function kelintas (){
+   static $k =0;
+    $k++;
+    return $k;
+}
+
+echo kelintas();
+echo kelintas();
+echo kelintas();
+echo br.'----------------------'.br;
+
+$bevardo =function(){
+    echo 'as neturiu vardo';
+};
+
+$bevardo();
