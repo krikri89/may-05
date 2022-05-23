1aa79e40cb71cb0d7c2e264d2e8170d713c514e5
Kristina Rudyte <rkristina32@yahoo.com>
2022-05-23 17:51:52 +0300

    nd react list 1st task

---
+++ ndreactlists/src/Components/011/Daiktas.jsx
@@ -0,0 +1,8 @@
+function Daiktas({ seaPlaner }) {
+  return (
+    <div>
+      id: <i>{seaPlaner.id}</i> <b>{seaPlaner.name}</b>
+    </div>
+  );
+}
+export default Daiktas;
---
+++ ndreactlists/src/Components/011/Tvenkinys.jsx
@@ -0,0 +1,17 @@
+import Daiktas from "./Daiktas";
+
+function Tvenkinys ({seaPlaners}){
+    return (
+        <>          
+{
+    seaPlaners.map(s => s.id % 2 ? <Daiktas key={s.id} seaPlaner={s}></Daiktas> : null)
+}
+<hr/>
+{
+    seaPlaners.map(s => s.id % 2 ? null: <Daiktas key={s.id} seaPlaner={s}></Daiktas>)
+}
+</>
+    )
+}
+
+export default Tvenkinys;
