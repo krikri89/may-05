1c2cd021de1c9c1b9123fb1907519f569144da35
Kristina Rudyte <rkristina32@yahoo.com>
2022-05-25 13:55:49 +0300

    workshop

---
+++ may25/src/App.jsx
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import Animals from './Components/Animals/Animals';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div><Animals/></div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
