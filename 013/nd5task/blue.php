8e67bcd7d43efcb7fa81921137d1d9c02493e262
Kristina Rudyte <rkristina32@yahoo.com>
2022-05-26 09:36:10 +0300

    nd web mechanika 5task

---
+++ 013/nd5task/blue.php
@@ -0,0 +1,19 @@
+<?php
+if(isset($_GET['r'])){
+    header ('location: http://localhost/vienaragiai/may-05/013/nd5task/red.php');
+die;
+};
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blue</title>
+
+</head >
+<body style = "background:blue;">
+    <a href="http://localhost/vienaragiai/may-05/013/nd5task/blue.php?r=1">RED</a>
+</body>
+</html>
---
+++ 013/nd5task/red.php
@@ -0,0 +1,18 @@
+<?php
+if(isset($_GET['r'])){
+    header ('Location: http://localhost/vienaragiai/may-05/013/nd5task/blue.php');
+die;
+};
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RED</title>
+</head >
+<body style = "background:red;">
+    <a href="http://localhost/vienaragiai/may-05/013/nd5task/red.php?r=1">BLUE</a>
+</body>
+</html>
