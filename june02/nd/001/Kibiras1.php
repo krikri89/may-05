eeff72ab35dfd7d6c2f09e40b20baca7747b934e
Kristina Rudyte <rkristina32@yahoo.com>
2022-06-02 15:26:54 +0300

    kibiras

---
+++ june02/nd/001/Kibiras1.php
@@ -0,0 +1,10 @@
+<!-- Sukurti klasę Kibiras1. Sukurti protected savybę akmenuKiekis. Parašyti šiai savybei metodus prideti1Akmeni() pridetiDaugAkmenu($kiekis) ir metodą kiekPririnktaAkmenu(). Sukurti kibiro objektą ir pademonstruoti akmenų rinkimą į kibirą ir rezultatų išvedimą.
+ -->
+<?php
+class Kibiras1
+{
+    protected $akmenuKiekis;
+    public function prideti1Akmeni(){
+        
+    }
+}
---
+++ june02/nd/001/index.php
@@ -0,0 +1,4 @@
+<?php
+require __DIR__ . '/Kibiras1.php';
+
+$Kibiras1 = new Kibiras1;
