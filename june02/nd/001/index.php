dbfbd0ae44061c519cc1565a16535cf7445d1be1
Kristina Rudyte <rkristina32@yahoo.com>
2022-06-02 23:32:50 +0300

    ok

--- june02/nd/001/index.php
+++ june02/nd/001/index.php
@@ -5,7 +5,7 @@ $Kibiras1 = new Kibiras1;
 echo '<pre>';
 echo '<br>';
 
-// echo $Kibiras1->prideti1Akmeni;
-echo $Kibiras1->akmenuKiekis;
+echo $Kibiras1->prideti1Akmeni;
+
 echo '<br>';
 echo $Kibiras1->pridetiDaugAkmenu(12);
