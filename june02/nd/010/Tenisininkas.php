8ae6f49c800fbdfb227dda92f8006e19d46202d0
Kristina Rudyte <rkristina32@yahoo.com>
2022-06-06 09:41:50 +0300

    nd 10 tenisininkas

--- june02/nd/010/Tenisininkas.php
+++ june02/nd/010/Tenisininkas.php
@@ -8,4 +8,46 @@ Sukurti du Tenisininkas objektus. Kamuoliuko neturi nei vienas. Iškviesti stati
 
 class Tenisininkas
 {
+    private $vardas;
+    private $kamuoliukas;
+    private static $zaidejas1;
+    private static $zaidejas2;
+
+    public static function zaidimoPradzia()
+    {
+        if (null === self::$zaidejas1 || null === self::$zaidejas2) {
+            echo 'nesusirinko pakankamai zaideju';
+        }
+        self::$zaidejas1->kamuoliukas = (bool)rand(0, 1);
+        self::$zaidejas2->kamuoliukas = !self::$zaidejas1->kamuoliukas;
+    }
+    public function __construct($name)
+    {
+        $this->name = $name;
+        if (null === self::$zaidejas1) {
+            self::$zaidejas1 = $this;
+        } elseif (null === self::$zaidejas2) {
+            self::$zaidejas2 = $this;
+        }
+    }
+    public function arTuriKamuoliuka()
+    {
+        return $this->kamuoliukas;
+    }
+    public function perduotiKamuoliuka()
+    {
+        if (!$this->arTuriKamuoliuka) {
+            echo '<h3>' . $this->name . ' Neturi kamuoliuko</h3>';
+        } else {
+            if (self::$zaidejas1->arTuriKamuoliuka()) {
+                self::$zaidejas2->kamuoliukas = true;
+                $this->kamuoliukas = false;
+                echo '<h3>Kamuoliukas perduotas</h3>';
+            } else {
+                self::$zaidejas1->kamuoliukas = true;
+                $this->kamuoliukas = false;
+                echo '<h3>Kamuoliukas perduotas</h3>';
+            }
+        }
+    }
 }
--- june02/nd/010/index.php
+++ june02/nd/010/index.php
@@ -1,5 +1,18 @@
 <?php
 
-require __DIR__ . 'Tenisininkas.php';
+require __DIR__ . '/Tenisininkas.php';
 echo '<pre>';
 echo '<br>';
+
+$t1 = new Tenisininkas('Diedas');
+$t2 = new Tenisininkas('Teta');
+
+Tenisininkas::zaidimoPradzia();
+
+print_r($t1);
+print_r($t2);
+
+$t1->perduotiKamuoliuka();
+
+print_r($t1);
+print_r($t2);
