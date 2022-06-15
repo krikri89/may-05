ca8f6b69865623068f7abc060ddfad06aa7754b8
Kristina Rudyte <rkristina32@yahoo.com>
2022-06-15 11:13:53 +0300

    cache

---
+++ june14/Cache.php
@@ -0,0 +1,46 @@
+<?php
+
+class Cache
+{
+    private $time;
+    private $valid = 30;
+
+    public function __construct()
+    {
+        $this->time = time();
+    }
+
+    // 55-15 = 40 < 30 false
+
+    private function isValid($dataCacheTime)
+    {
+        return $this->time - $dataCacheTime < $this->valid;
+    }
+
+    private function getCacheTime()
+    {
+        return $_SESSION['cache_time'] ?? 0;
+    }
+
+    private function write($data)
+    {
+        $_SESSION['cache_data'] = $data;
+        $_SESSION['cache_time'] = $this->time;
+    }
+    private function read()
+    {
+        return $_SESSION['cache_data'] ?? null;
+    }
+    public function get()
+    {
+        $dataCacheOn = $this->getCacheTime();
+        if (!$this->isValid($$dataCacheOn)) {
+            return null;
+        }
+        return $this->read;
+    }
+    public function set($data)
+    {
+        $this->write($data);
+    }
+}
--- june14/index.php
+++ june14/index.php
@@ -1,7 +1,28 @@
 <?php
+
+require __DIR__ . '/Cache.php';
+
 session_start();
+$cache = new Cache;
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $_SESSION['visual'] = 'CACHE';
+    $output = $cache->get();
+    if (null === $output) {
+        $_SESSION['visual'] = 'LIVE';
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, 'https://www.distance24.org/route.json?stops=' . $_POST['from'] . '|' . $_POST['to']);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        $output = curl_exec($ch); // siunciam, laukiam, gaunam
+        curl_close($ch); // uzdarom duris 
+        $output = json_decode($output);
+        $cache->set($output);
+    }
+
+
     $ch = curl_init();
 
     curl_setopt($ch, CURLOPT_URL, 'https://www.distance24.org/route.json?stops=' . $_POST['from'] . '|' . $_POST['to']);
@@ -19,9 +40,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     $dist = $_SESSION['dist'] ?? '';
     $img1 = $_SESSION['img1'] ?? '';
+    $visual = $_SESSION['visual'] ?? 'dar nera visual';
 
-    unset($_SESSION['dist'], $_SESSION['img1']);
-?>
+    unset($_SESSION['dist'], $_SESSION['img1'], $_SESSION['visual']);
+?> <h1 style="color: red;"><?= $visual ?></h1>
     <form action="" method="post">
         from: <input type="text" name="from">
         to: <input type="text" name="to">
