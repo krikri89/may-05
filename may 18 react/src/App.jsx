e9143817e24984cfaa501a30b9ba7f4973eaf445
Kristina Rudyte <rkristina32@yahoo.com>
2022-06-30 13:46:54 +0300

    ck

--- may 18 react/src/App.jsx
+++ may 18 react/src/App.jsx
@@ -1,70 +1,62 @@
 import { useState } from 'react';
 import './App.css';
-import Hello from './Components/009/Hello';
 // import Hello from './Components/009/Hello';
 // import Briedis from './Components/009/Briedis';
-// import Zuikis from './Components/homework/01reactbase';
-// import Mike from './Components/homework/02';
-// import Zebrai from './Components/homework/03Zebrai';
-// import Barsukai from './Components/homework/04';
-// import Keturi from './Components/homework/05';
-import randColor from './Functions/randColors';
+// import randColor from './Functions/randColors';
 
 function App() {
-  // const [spalva, setSpalva] = useState('blue');
+  const [spalva, setSpalva] = useState('blue');
+
   // const [number, setNumber] = useState(1);
   // const cats = ['Pilkis', 'Murkis', 'rainis'];
-  const [kv, setKv] = useState([]);
+  // const [kv, setKv] = useState([]);
 
-  // const stebuklas = (a) => {
-  // console.log('stebuklu stebuklas' + a);
-  // setNumber(number + 1);
-  // };
+  const stebuklas = (a) => {
+    console.log('stebuklu stebuklas' + a);
+    // setNumber(number + 1);
+  };
+  const kitasStebuklas = () => {
+    console.log(' stebuklu stebuklas');
+    setSpalva('red');
+  };
 
-  // const kitasStebuklas = () => {
-  // console.log(' stebuklu stebuklas');
-  // setSpalva((oldColor) => (oldColor === 'yellow' ? 'blue' : 'yellow'));
+  setSpalva((oldColor) => (oldColor === 'yellow' ? 'blue' : 'yellow'));
+
   // setSpalva('yellow');
-  // };
-
   // const skaicius = () => {
   //   setNumber(number + 1);
   // };
 
-  const addKv = () => setKv((kvM) => [...kvM, randColor()]);
-  const REMkv = () => setKv((kvM) => kvM.slice(1));
+  // const addKv = () => setKv((kvM) => [...kvM, randColor()]);
+  // const REMkv = () => setKv((kvM) => kvM.slice(1));
 
   return (
     <div className="App">
       <header className="App-header">
-        <Hello/>
+        <h1 style={{ color: spalva }}>State</h1>
 
+        <button onClick={() => stebuklas('Abra-cadabra')}>Press with!</button>
+        <button onClick={kitasStebuklas}>Press W/O</button>
 
-        {/* <h1 style={{ color: spalva }}>State</h1> */}
-        {/* <button onClick={() => stebuklas('Abra-cadabra')}>Press with!</button> */}
-        {/* <button onClick={kitasStebuklas}>Press W/O</button> */}
-        {
+        {/* {
           <div className="kvc">
             {kv.map((c, i) => (
               <div key={i} className="kv" style={{ background: c }}></div>
             ))}
           </div>
-        }
+        } */}
+
         <div className="cssround">round or not</div>
-        <button onClick={addKv}>-- press---</button>
-        <button onClick={REMkv}>-- remove---</button>
+        {/* <button onClick={addKv}>-- press---</button> */}
+        {/* <button onClick={REMkv}>-- remove---</button> */}
+
         {/* <button onClick={skaicius}>{number}</button> */}
         {/* {cats.map((cat, i) => ( */}
         {/* <div key={i}>{cat}</div> */}
         {/* ))} */}
 
-        {/* <Hello spalva="pink" size="14" skaicius={3}></Hello>
-        <Briedis></Briedis>
-        <Zuikis />
-        <Mike text="bla" size="45" />
-        <Zebrai prop={2} />
-        <Barsukai bold="Barsukai miega ziema" thin="Barsukai nemegsta sesku" />
-        <Keturi text1="Keturi broliai" text2="3seserys" color="yellow" /> */}
+        {/* <Hello spalva="pink" size="14" skaicius={3}></Hello> */}
+        {/* <Briedis></Briedis> */}
       </header>
     </div>
   );
