041d7f0b18d301726867527a48c9722802a64e05
Kristina Rudyte <rkristina32@yahoo.com>
2022-07-01 11:06:18 +0300

    data

--- may24priedas/index.php
+++ may24priedas/index.php
@@ -9,7 +9,10 @@ $cats = [
 $out = json_encode($cats);
 
 
-header('Content-Type: application/json'); // svarbiausia kad header butu auksciau negu body
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, DELETE, PUT');
+header("Access-Control-Allow-Headers: X-Requested-With");
 
 
 
--- may24r/src/App.jsx
+++ may24r/src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
@@ -10,14 +10,24 @@ function App() {
     console.log('go');
   }, []);
 
+  useEffect(() => {
+    axios
+      .get('http://localhost/vienaragiai/may-05/may24priedas/') // is kur gaunam data
+      .then((res) => {
+        // ka daryti su data. res=response
+        console.log(res.data); // siuo atveju tik atspausdinti data i console.
+        setCats(res.data); // spausdins i narsykle
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>{count}</h1>
         <button onClick={() => setCount((c) => c + 1)}>+1</button>
-      {
-        cats.map((cats, index) => <div key = {index}>{cats}</div>)
-      }
+        {cats.map((cats, index) => (
+          <div key={index}>{cats}</div>
+        ))}
       </header>
     </div>
   );
