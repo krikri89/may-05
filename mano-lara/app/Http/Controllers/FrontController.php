398ca665c9ce1a9210fb28644f7038be6b32b7cd
Kristina Rudyte <rkristina32@yahoo.com>
2022-07-19 21:31:43 +0300

    filter

--- mano-lara/app/Http/Controllers/FrontController.php
+++ mano-lara/app/Http/Controllers/FrontController.php
@@ -12,35 +12,39 @@ class FrontController extends Controller
     public function index(Request $request)
     {
 
-        $animals = match ($request->sort) {
-            'color-asc' => DB::table('animals')
+        $animalsDir = match ($request->sort) {
+            'color-asc' => [DB::table('animals')
                 ->join('colors', 'colors.id', '=', 'animals.color_id') // colors.id ->is lenteles colors paimam id ir sujungiam su kitos id
                 ->select('colors.*', 'animals.*') //* viska is abieju lenteliu
                 ->orderBy('colors.title', 'asc')
-                ->get(),
-            'color-desc' => DB::table('animals')
+                ->get(), 'color-asc'],
+            'color-desc' => [DB::table('animals')
                 ->join('colors', 'colors.id', '=', 'animals.color_id')
                 ->select('colors.*', 'animals.*')
                 ->orderBy('colors.title', 'desc')
-                ->get(),
-            'animal-asc' => DB::table('animals')
+                ->get(), 'color-desc'],
+            'animal-asc' => [DB::table('animals')
                 ->join('colors', 'colors.id', '=', 'animals.color_id')
                 ->select('colors.*', 'animals.*')
                 ->orderBy('animals.name', 'asc') //pagal animals name
                 ->orderBy('colors.title', 'asc')
-                ->get(),
-            'animal-desc' => DB::table('animals')
+                ->get(), 'animal-asc'],
+            'animal-desc' => [DB::table('animals')
                 ->join('colors', 'colors.id', '=', 'animals.color_id')
                 ->select('colors.*', 'animals.*')
                 ->orderBy('animals.name', 'desc')
                 ->orderBy('colors.title', 'desc')
-                ->get(),
+                ->get(), 'animal-desc'],
 
-            default => DB::table('animals')
+            default => [DB::table('animals')
                 ->join('colors', 'colors.id', '=', 'animals.color_id')
                 ->select('animals.*', 'colors.*')
-                ->get()->shuffle(),
+                ->get()->shuffle(), 'default'],
         };
-        return view('front.index', ['animals' => $animals]);
+        return view('front.index', [
+            'animals' => $animalsDir[0],
+            'sort' => $animalsDir[1],
+            'colors' => Color::all(),
+        ]);
     }
 }
--- mano-lara/resources/views/front/box.blade.php
+++ mano-lara/resources/views/front/box.blade.php
@@ -3,27 +3,42 @@
         <h1>Sort Filter Search</h1>
     </div>
     <div class="card-body">
-        <div class="container">
-            <div class="row">
-                <div class="col-4">
-                    <form class="delete" action="{{route('front-index')}}" method="get">
+        <form class="delete" action="{{route('front-index')}}" method="get">
+            <div class="container">
+                <div class="row">
+                    <div class="col-3">
                         <div class="form-group">
                             <label>What sort?</label>
                             <select class="form-control" name="sort">
-                                <option value="dafault">Default sort</option>
-                                <option value="color-asc">Color A-Z</option>
-                                <option value="color-desc">Color Z-A</option>
-                                <option value="animals-asc">Animal A-Z</option>
-                                <option value="animal-desc">Animal Z-A</option>
+                                <option value="dafault" @if($sort=='default' )selected @endif>Default sort</option>
+                                <option value="color-asc" @if($sort=='color-asc' )selected @endif>Color A-Z</option>
+                                <option value="color-desc" @if($sort=='color-desc' )selected @endif>Color Z-A</option>
+                                <option value="animals-asc" @if($sort=='animals-asc' )selected @endif>Animal A-Z</option>
+                                <option value="animal-desc" @if($sort=='animal-desc' )selected @endif>Animal Z-A</option>
                             </select>
                         </div>
-                        <button class="btn btn-outline-warning m-2" type="submit">Sort</button>
-                    </form>
-                    <a class="btn btn-outline-primary m-2" href="{{route('front-index')}}">Clear</a>
+                        <button class="btn btn-outline-warning mt-2" type="submit">Sort</button>
+                    </div>
+                    <div class="col-3">
+                        <div class="form-group">
+                            <label>What color?</label>
+                            <select class="form-control" name="color_id">
+                                @foreach($colors as $color)
+                                <option value="{{$color->id}}">{{$color->title}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <button class="btn btn-outline-success mt-2" type="submit">Filter</button>
+                    </div>
+                    <div class="col-4">
 
 
+                    </div>
+                    <div class="col-1">
+                        <a class="btn btn-outline-primary mt-2" href="{{route('front-index')}}">Clear</a>
+                    </div>
                 </div>
             </div>
-        </div>
+        </form>
     </div>
 </div>
